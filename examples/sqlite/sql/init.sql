175ca34311f48b5441e6c0370c175b846efc4d55
3D Nda <hn63wospuvy@gmail.com>
2024-07-09 23:52:50 +0700

    update examples script

---
+++ examples/sqlite/sql/init.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    org INTEGER,
+    active BOOLEAN NOT NULL,
+    created_by TEXT,
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+    updated_by TEXT,
+    updated_at TEXT
+);
+
+CREATE TABLE IF NOT EXISTS chats (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    sender INTEGER NOT NULL,
+    receiver INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    active BOOLEAN NOT NULL,
+    created_by TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS organizations (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    code TEXT NOT NULL,
+    image TEXT,
+    active BOOLEAN NOT NULL,
+    created_by TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_by TEXT,
+    updated_at DATETIME
+);
